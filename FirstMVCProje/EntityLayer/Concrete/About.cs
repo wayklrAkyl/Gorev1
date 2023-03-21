5c6bcfa8898bb4b9c69156d0efd2d866fba2c355
wayklrAkyl <127725897+wayklrAkyl@users.noreply.github.com>
2023-03-21 22:26:46 +0300

    Gorev1

---
+++ FirstMVCProje/EntityLayer/Concrete/About.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        [Key]
+        public int AboutID { get; set; }
+        [StringLength(1000)]
+        public string AboutDetails1 { get; set; }
+        [StringLength(1000)]
+        public string AboutDetails2 { get; set; }
+
+        [StringLength(100)]
+        public string AboutImage { get; set; }
+        [StringLength(100)]
+        public string AboutImag2 { get; set; }
+    }
+}
---
+++ FirstMVCProje/FirstMVCProje/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstMVCProje.MvcApplication" Language="C#" %>
